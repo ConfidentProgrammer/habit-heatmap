5e9ab436cbadc9eeda506d706b28889107b6de3a
ConfidentProgrammer <deepp2618@gmail.com>
2025-09-16 07:14:29 -0400

    some UI changes

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -1,6 +1,6 @@
 import Tag from './Tag';
 import Year from './Year';
-
+import check from '../assets/check-circle.svg';
 interface IHabitProps {
   toggleDay: (id: number, date: Date) => void;
   id: number;
@@ -23,13 +23,18 @@ const Habit = ({ toggleDay, id, habit }: IHabitProps) => {
         <Tag text="28 Day streak" />
       </div>
 
-      <button
-        className="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
-        onClick={() => toggleDay(id, todaysDate)}
-      >
-        Daily Check-in: {todaysDate.toDateString()}
-      </button>
-      <Year progress={habit.progress} />
+      <div className="year-container flex justify-between">
+        <Year progress={habit.progress} />
+        <div className="btn-container flex flex-col justify-end">
+          <button
+            className="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-4xl text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 flex"
+            onClick={() => toggleDay(id, todaysDate)}
+          >
+            Mark Done
+            <img className="ml-1" src={check} height={15} width={15} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
